b07ae63a578615d33e0ace3dc0dbe92f0a459dcc
sue445 <sue445@sue445.net>
2015-08-17 19:05:13 +0900

    mix phoenix.new hello_phoenix --database mysql

---
+++ config/config.exs
@@ -0,0 +1,24 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  url: [host: "localhost"],
+  root: Path.dirname(__DIR__),
+  secret_key_base: "oxoFbv3r4jyMx84/IPG1XaZD5bQijfrf5Q+qCZEajjs+Zxt/2XdheOeU55h/otQv",
+  render_errors: [default_format: "html"],
+  pubsub: [name: HelloPhoenix.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
